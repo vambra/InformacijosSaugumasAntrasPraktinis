898d592d3f6108fbcda32420a8a132a352181dde
Bri <st.brigita@gmail.com>
2022-03-14 16:24:41 +0200

    Implemented read from file and save to file functions

--- antrasPraktinis/Form1.Designer.cs
+++ antrasPraktinis/Form1.Designer.cs
@@ -31,98 +31,149 @@ namespace antrasPraktinis
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buttonFileSave = new System.Windows.Forms.Button();
+            this.buttonFileRead = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 54);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 29);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(776, 100);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 168);
+            this.label1.Location = new System.Drawing.Point(12, 145);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(36, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Key:";
             // 
-            // button1
+            // buttonEncrypt
             // 
-            this.button1.Location = new System.Drawing.Point(272, 168);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Encrypt";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Location = new System.Drawing.Point(632, 138);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 30);
+            this.buttonEncrypt.TabIndex = 2;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
             // 
-            // button2
+            // buttonDecrypt
             // 
-            this.button2.Location = new System.Drawing.Point(354, 167);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Decrypt";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Location = new System.Drawing.Point(713, 138);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(75, 30);
+            this.buttonDecrypt.TabIndex = 3;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(12, 219);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 210);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.ReadOnly = true;
-            this.richTextBox2.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(776, 100);
             this.richTextBox2.TabIndex = 4;
             this.richTextBox2.Text = "";
             // 
-            // button3
+            // buttonFileSave
             // 
-            this.button3.Location = new System.Drawing.Point(446, 356);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(120, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "Save to file";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonFileSave.Location = new System.Drawing.Point(668, 316);
+            this.buttonFileSave.Name = "buttonFileSave";
+            this.buttonFileSave.Size = new System.Drawing.Size(120, 30);
+            this.buttonFileSave.TabIndex = 5;
+            this.buttonFileSave.Text = "Save to file";
+            this.buttonFileSave.UseVisualStyleBackColor = true;
+            this.buttonFileSave.Click += new System.EventHandler(this.buttonFileSave_Click);
             // 
-            // button4
+            // buttonFileRead
             // 
-            this.button4.Location = new System.Drawing.Point(217, 356);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(120, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "Read from file";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonFileRead.Location = new System.Drawing.Point(542, 316);
+            this.buttonFileRead.Name = "buttonFileRead";
+            this.buttonFileRead.Size = new System.Drawing.Size(120, 30);
+            this.buttonFileRead.TabIndex = 6;
+            this.buttonFileRead.Text = "Read from file";
+            this.buttonFileRead.UseVisualStyleBackColor = true;
+            this.buttonFileRead.Click += new System.EventHandler(this.buttonFileRead_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(81, 168);
+            this.textBox1.Location = new System.Drawing.Point(54, 142);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(300, 22);
             this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Initial text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Encrypted/Decrypted text:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "ECB",
+            "CBC",
+            "CFB"});
+            this.comboBox1.Location = new System.Drawing.Point(437, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(384, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Mode:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonFileRead);
+            this.Controls.Add(this.buttonFileSave);
             this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AES";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,12 +183,16 @@ namespace antrasPraktinis
 
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
         private System.Windows.Forms.RichTextBox richTextBox2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonFileSave;
+        private System.Windows.Forms.Button buttonFileRead;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- antrasPraktinis/Form1.cs
+++ antrasPraktinis/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace antrasPraktinis
 {
@@ -15,6 +16,41 @@ namespace antrasPraktinis
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = richTextBox1.Text;
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = richTextBox1.Text;
+        }
+
+        private void buttonFileRead_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Choose text file";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void buttonFileSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            saveFileDialog1.Title = "Save text to file";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, richTextBox2.Text);
+            }
+
         }
     }
 }
